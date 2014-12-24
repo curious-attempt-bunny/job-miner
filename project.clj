bb890b6f81f0a34571b0be4bc40a420a7ac6ed28
Merlyn <curious.attempt.bunny@gmail.com>
2014-12-23 22:27:20 -0800

    Mine Puppet Labs.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject jobagator2015 "0.1"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [enlive "1.1.5"]
-                 [clj-http "1.0.1"]])
+                 [clj-http "1.0.1"]
+                 [cheshire "5.4.0"]])
---
+++ src/jobagator2015/puppet_labs.clj
@@ -0,0 +1,11 @@
+(def site "https://frozen-forest-9048.herokuapp.com")
+(def site "http://localhost:5000")
+
+(require '[clj-http.client :as client]
+         '[cheshire.core :as json])
+
+(def content (:body (client/get "http://www.jobscore.com/jobs/puppetlabs/widget_iframe?list_type=multi&parent_url=http%3A%2F%2Fpuppetlabs.com%2Fabout%2Fcareers&widget_id=js_widget_iframe_1")))
+
+(def job_data (->> (re-seq #"var jobs_data_preload = (.*?});" content) first last))
+
+(def entries (json/parse-string job_data))
