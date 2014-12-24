ff89a9d0862d93fd7721fd92940b6bca8da24ea3
Merlyn <curious.attempt.bunny@gmail.com>
2014-12-23 21:22:53 -0800

    Basic mining.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject jobagator2015 "0.1"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [enlive "1.1.5"]
+                 [clj-http "1.0.1"]])
---
+++ src/jobagator2015/silicon_florist.clj
@@ -0,0 +1,27 @@
+(ns jobagator2015.core)
+(def site "http://localhost:5000")
+(def site "https://frozen-forest-9048.herokuapp.com")
+
+(require '[net.cgrand.enlive-html :as html])
+(require '[clj-http.client :as client])
+
+(def content (html/html-resource (java.net.URL. "http://siliconflorist.com/jobs/")))
+(def entries (html/select content [:table#wpjb_jobboard :tr]))
+
+(->> entries
+     (map #(first (html/select % [:td :a])))
+     (remove nil?)
+     (map #(array-map
+                :title (first (:content %))
+                :url   (-> % :attrs :href)))
+     (map #(str
+            site
+            "/jobs?url="
+            (java.net.URLEncoder/encode (:url %))
+            "&title="
+            (java.net.URLEncoder/encode (:title %))))
+     (pmap #(client/post %))
+)
+
+
+
