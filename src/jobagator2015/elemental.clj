86a13284919caaec7dbe6f025b7540371e9a73ca
Merlyn <curious.attempt.bunny@gmail.com>
2014-12-24 00:26:23 -0800

    Really mine elemental.

--- src/jobagator2015/elemental.clj
+++ src/jobagator2015/elemental.clj
@@ -1,20 +1,16 @@
-http://www.elementaltechnologies.com/company/careers/open-positions
-
 (def site "https://frozen-forest-9048.herokuapp.com")
 (def site "http://localhost:5000")
 
 (require '[net.cgrand.enlive-html :as html])
 
-(def content (html/html-resource (java.net.URL. "http://hire.jobvite.com/CompanyJobs/Careers.aspx?k=JobListing&c=q6M9VfwG&v=1&jvresize=http://newrelic.com/FrameResize.html")))
-(def entries (html/select content [:.joblist :li]))
+(def content (html/html-resource (java.net.URL. "http://elementaltechnologies.theresumator.com/apply/jobs/")))
+(def entries (html/select content [:a.job_title_link]))
 
 (->> entries
-    (filter #(not-empty (html/select % [:a.jvjoblink])))
-    (filter #(= "Portland, OR, United States" (first (:content (last (html/select % [:span]))))))
-    (map #(first (html/select % [:a.jvjoblink])))
+    (map #(first (html/select % [:a])))
     (map #(array-map
             :title (clojure.string/trim (first (:content %)))
-            :url   (-> % :attrs :href)))
+            :url   (str "http://elementaltechnologies.theresumator.com" (-> % :attrs :href))))
     (map #(str
         site
         "/jobs?url="
