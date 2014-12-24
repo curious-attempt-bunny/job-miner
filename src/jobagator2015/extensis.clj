f19762b5e5a8ba3e380d6fafe85f2878d8971137
Merlyn <curious.attempt.bunny@gmail.com>
2014-12-23 23:28:20 -0800

    Better extensis mining.

--- src/jobagator2015/extensis.clj
+++ src/jobagator2015/extensis.clj
@@ -4,30 +4,23 @@
 (require '[net.cgrand.enlive-html :as html])
 
 (def content (html/html-resource (java.net.URL. "http://ch.tbe.taleo.net/CH01/ats/careers/searchResults.jsp?org=EXTENSIS&cws=1")))
-(def entries (html/select content [:tr]))
+(def entries (html/select content [:table#cws-search-results :tr]))
 
 (->> entries
-    (filter (fn [el] (some #(= "Portland, OR" %) (first (map :content (html/select el [:td :b]))))))
-    )
-
-(map
-    (fn [el] 
-        (some
-            #(= "Portland, OR" (first %))
-            (map :content (html/select el [:td :b]))))
-    entries)
-    
-
-    (filter #(= "Portland, OR" (first (:content (last (html/select % [:td :b]))))))
-    ; (map #(first (html/select % [:a])))
-    ; (map #(array-map
-    ;         :title (clojure.string/trim (first (:content %)))
-    ;         :url   (-> % :attrs :href)))
-    ; (map #(str
-    ;     site
-    ;     "/jobs?url="
-    ;     (java.net.URLEncoder/encode (:url %))
-    ;     "&title="
-    ;     (java.net.URLEncoder/encode (:title %))))
-    ; (pmap #(client/post %))
+    (filter
+        (fn [el] 
+            (some
+                #(= "Portland, OR" (first %))
+                (map :content (html/select el [:td :b])))))
+    (map #(first (html/select % [:a])))
+    (map #(array-map
+            :title (clojure.string/trim (first (:content %)))
+            :url   (-> % :attrs :href)))
+    (map #(str
+        site
+        "/jobs?url="
+        (java.net.URLEncoder/encode (:url %))
+        "&title="
+        (java.net.URLEncoder/encode (:title %))))
+    (pmap #(client/post %))
 )
