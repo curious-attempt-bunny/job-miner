b65b204072a65d825c06a5fc165d7a5c19879f28
Merlyn <curious.attempt.bunny@gmail.com>
2014-12-23 23:06:42 -0800

    Mine Extensis.

---
+++ src/jobagator2015/extensis.clj
@@ -0,0 +1,33 @@
+(def site "https://frozen-forest-9048.herokuapp.com")
+(def site "http://localhost:5000")
+
+(require '[net.cgrand.enlive-html :as html])
+
+(def content (html/html-resource (java.net.URL. "http://ch.tbe.taleo.net/CH01/ats/careers/searchResults.jsp?org=EXTENSIS&cws=1")))
+(def entries (html/select content [:tr]))
+
+(->> entries
+    (filter (fn [el] (some #(= "Portland, OR" %) (first (map :content (html/select el [:td :b]))))))
+    )
+
+(map
+    (fn [el] 
+        (some
+            #(= "Portland, OR" (first %))
+            (map :content (html/select el [:td :b]))))
+    entries)
+    
+
+    (filter #(= "Portland, OR" (first (:content (last (html/select % [:td :b]))))))
+    ; (map #(first (html/select % [:a])))
+    ; (map #(array-map
+    ;         :title (clojure.string/trim (first (:content %)))
+    ;         :url   (-> % :attrs :href)))
+    ; (map #(str
+    ;     site
+    ;     "/jobs?url="
+    ;     (java.net.URLEncoder/encode (:url %))
+    ;     "&title="
+    ;     (java.net.URLEncoder/encode (:title %))))
+    ; (pmap #(client/post %))
+)
