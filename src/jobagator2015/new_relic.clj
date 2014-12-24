09ba5d5e9f18027b7cd14213542ca21a9bba390c
Merlyn <curious.attempt.bunny@gmail.com>
2014-12-23 22:26:55 -0800

    Fix mining New Relic.

--- src/jobagator2015/new_relic.clj
+++ src/jobagator2015/new_relic.clj
@@ -5,14 +5,14 @@
 (require '[clj-http.client :as client])
 
 (def content (html/html-resource (java.net.URL. "http://hire.jobvite.com/CompanyJobs/Careers.aspx?k=JobListing&c=q6M9VfwG&v=1&jvresize=http://newrelic.com/FrameResize.html")))
-(def entries (html/select content [:.joblist :tr]))
+(def entries (html/select content [:.joblist :li]))
 
 (->> entries
     (remove #(nil? (html/select % [:a.jvjoblink])))
-    (filter #(= "Portland" (first (:content (last (html/select % [:td]))))))
+    (filter #(= "Portland, OR, United States" (first (:content (last (html/select % [:span]))))))
     (map #(first (html/select % [:a.jvjoblink])))
     (map #(array-map
-            :title (first (:content %))
+            :title (clojure.string/trim (first (:content %)))
             :url   (-> % :attrs :href)))
     (map #(str
         site
