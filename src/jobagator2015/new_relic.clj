0b40f0e46f721f5e04255947d7ee85bb6f9dfdab
Merlyn <curious.attempt.bunny@gmail.com>
2015-04-18 11:38:48 -0700

    Refactor.

--- src/jobagator2015/new_relic.clj
+++ src/jobagator2015/new_relic.clj
@@ -1,10 +1,10 @@
 (ns jobagator2015.new_relic
-    (:require [jobagator2015.core :refer :all]))
+    (:require [jobagator2015.core :refer [get-html select submit]]))
 
 (->> (get-html "http://hire.jobvite.com/CompanyJobs/Careers.aspx?k=JobListing&c=q6M9VfwG&v=1&jvresize=http://newrelic.com/FrameResize.html")
     (select [:.joblist :li])
-    (remove #(nil? (html/select % [:a.jvjoblink])))
-    (filter #(= "Portland, OR, United States" (first (:content (last (html/select % [:span]))))))
-    (map #(first (html/select % [:a.jvjoblink])))
+    (remove #(nil? (select [:a.jvjoblink] %)))
+    (filter #(= "Portland, OR, United States" (first (:content (last (select [:span] %))))))
+    (map #(first (select [:a.jvjoblink] %)))
     (submit)
 )
